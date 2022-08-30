21af43ce177cfc742b85b382ea3b57c6e3771c82
GatmanVitalie <95142233+GatmanVitalie@users.noreply.github.com>
2022-08-30 14:55:53 +0300

    Add files via upload

---
+++ HelloWord.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+  
+// Main() function: where the execution of program begins
+int main()
+{
+    int a = 1;
+    int b = 2;
+    int c = 0;
+    c = a;
+    a = b;
+    b = c;
+
+    cout <<"a="<<a<< endl;
+    cout <<"b="<<b<< endl;
+
+
+    return 0;
+   
+}
